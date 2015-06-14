ecee4ec5e56f9bcdba1d161827f976c0034e48ab
Trent Childers <trent65@msn.com>
2015-06-14 13:34:36 -0500

    added category and categorized product dbs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611013021) do
+ActiveRecord::Schema.define(version: 20150614183337) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "categorized_products", force: :cascade do |t|
+    t.integer  "record_id",   limit: 4
+    t.integer  "category_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "images", force: :cascade do |t|
     t.string   "image",      limit: 255
